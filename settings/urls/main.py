84b32706699b7834352345d8da5083b0c2915be6
Dustin Sallings <dustin@spy.net>
2005-07-23 23:16:01 -0700

    only go to the home page if it's requested
    darcs-hash:20050724061601-c4430-90e9134e1166070c85d3969dc62a5c3b78b8a758.gz

--- settings/urls/main.py
+++ settings/urls/main.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('',
     # Example:
     # (r'^scsf/', include('scsf.apps.foo.urls.foo')),
     (r'^page/(?P<page>[A-z]+)', 'scsf.apps.main.views.index.page'),
-    (r'', 'scsf.apps.main.views.index.page', {'page': 'index'}),
+    (r'^$', 'scsf.apps.main.views.index.page', {'page': 'index'}),
 )
