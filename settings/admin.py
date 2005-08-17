b6ec10fcd131dc7e0ec081f737807b0fd31f7216
Dustin Sallings <dustin@spy.net>
2005-08-16 23:33:19 -0700

    config changes for new django
    darcs-hash:20050817063319-c4430-f8071616b03cf3de519635d627179a8c8f156381.gz

--- settings/admin.py
+++ settings/admin.py
@@ -9,6 +9,7 @@ TEMPLATE_DIRS = (
 )
 ROOT_URLCONF = 'scsf.settings.urls.admin'
 MIDDLEWARE_CLASSES = (
+    'django.middleware.sessions.SessionMiddleware',
     'django.middleware.admin.AdminUserRequired',
     'django.middleware.common.CommonMiddleware',
 )
