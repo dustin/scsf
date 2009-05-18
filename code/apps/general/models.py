69dcd0516b255f22fc81cc1e7ad680b4f6023c13
Dustin Sallings <dustin@spy.net>
2009-05-18 15:32:43 -0700

    Compatibility fixes.

--- code/apps/general/models.py
+++ code/apps/general/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class School(models.Model):
-    name=models.CharField(maxlength=64)
-    address1=models.CharField(maxlength=64)
-    address2=models.CharField(maxlength=64, blank=True, null=True)
-    city=models.CharField(maxlength=64)
-    state=models.CharField(maxlength=64)
-    zip=models.CharField(maxlength=64)
-    phone=models.CharField(maxlength=64)
-    fax=models.CharField(maxlength=64)
+    name=models.CharField(max_length=64)
+    address1=models.CharField(max_length=64)
+    address2=models.CharField(max_length=64, blank=True, null=True)
+    city=models.CharField(max_length=64)
+    state=models.CharField(max_length=64)
+    zip=models.CharField(max_length=64)
+    phone=models.CharField(max_length=64)
+    fax=models.CharField(max_length=64)
 
     def __str__(self):
         return self.name
@@ -17,7 +17,7 @@ class School(models.Model):
         ordering = ['name']
 
 class Grade(models.Model):
-    name=models.CharField(maxlength=32)
+    name=models.CharField(max_length=32)
     seq=models.IntegerField()
 
     def __str__(self):
